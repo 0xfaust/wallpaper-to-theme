a990f8b795c28174ad2f8806ae31ff8fc6ccdd84
Albert Faust <robert.mcconnell3@mail.dcu.ie>
2015-06-18 16:01:35 +0100

    Update KMeans.c

--- KMeans.c
+++ KMeans.c
@@ -2,24 +2,62 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define CLUSTERS 3
-
-double distance(int dim, double *p1, double *p2)
+typedef struct RGB
 {
-        int i;
-        double d = 0; //lol breasts
-        for (i = 0; i < dim; i++){
-            d += ((p1[i] - p2[i]) * (p1[i] - p2[i]));
-        }
-        return d;
-}
-void all_distances(int dim, int n, int k, double *data, double *centroid, double *out)
-{
-        int i, j;
-        for (i = 0; i < n; i++){
-                for (j = 0; j < k; j++){
-                        out[i*k + j] = distance(dim, &data[i * dim], &centroid[j * dim]);
-                }
-        }
+        int red;
+        int green;
+        int blue;
+} rgb;
 
+int main()
+{
+    int pixels[16] = {123145178 
+                    , 173249173
+                    , 184230174
+                    , 192138191
+                    , 123251234
+                    , 212238175
+                    , 229172254
+                    , 192217195
+                    , 192248195
+                    , 228172248
+                    , 228229202
+                    , 235192231
+                    , 195212222
+                    , 213215212
+                    , 123212222
+                    , 231231232  };
+    
+    int centroids[3] = {123231132
+                    , 231123213
+                    , 213231123 };
+            
+    int i, j;
+    
+    float distance;
+
+    rgb colours[16];
+    rgb clusters[3];
+
+    for (i=0; i<16; i++){
+        
+        colours[i].red = floor(pixels[i]/1000000);
+        colours[i].green = abs(floor(pixels[i]/1000000)*1000-floor(pixels[i]/1000));
+        colours[i].blue = abs(floor(pixels[i]/1000)*1000-pixels[i]);
+        
+        clusters[i].red = floor(centroids[i]/1000000);
+        clusters[i].green = abs(floor(centroids[i]/1000000)*1000-floor(centroids[i]/1000));
+        clusters[i].blue = abs(floor(centroids[i]/1000)*1000-centroids[i]);
+
+    }
+
+    for (j=0; j<3; j++){
+        for (i=0; i<16; i++){
+
+            distance = sqrt((clusters[j].red - colours[i].red)*(clusters[j].red - colours[i].red) + (clusters[j].green - colours[i].green)*(clusters[j].green - colours[i].green) + (clusters[j].blue - colours[i].blue)*(clusters[j].blue - colours[i].blue));
+            printf("%f\n", distance);
+        }
+    }
+
+    return 0;
 }
