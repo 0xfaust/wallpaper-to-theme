dcc0040853a93bd5c176a89a58e65edf7dea250f
araic <ciara.mcauley4@mail.dcu.ie>
2015-06-20 18:47:38 +0100

    added RGB class to BufferedImaged

--- BufferedImaged.java
+++ BufferedImaged.java
@@ -5,7 +5,7 @@ import java.awt.Color;
 import java.io.IOException;
 
 public class BufferedImaged {
-	public static void main(String[] args){
+    public static void main(String[] args){
 		BufferedImage image = null;
 		try{
 			image = ImageIO.read(new File("test.jpg"));
@@ -14,13 +14,35 @@ public class BufferedImaged {
 		}
 		int w = image.getWidth();
 		int h = image.getHeight();
-		
+		int i = 0;
+        int pixels = w*h;
 		int[] dataBuffInt = image.getRGB(0,0,w,h,null,0,w);
-		
-		Color c = new Color(dataBuffInt[0]);
-		
-		System.out.println("Red: "+c.getRed());
-		System.out.println("Green: "+c.getGreen());
-		System.out.println("Blue: "+c.getBlue());
+
+        RGB[] colours = new RGB[pixels];
+
+        for (i=0; i<pixels; i++){
+		    Color c = new Color(dataBuffInt[i]);
+		    colours[i] = new RGB(c.getRed(),c.getGreen(),c.getBlue());
+        }
+        System.out.println("Red\tGreen\tBlue");
+        for (i=0; i<pixels; i++){
+        	RGB rgb = colours[i];
+            System.out.println(rgb.getR() + "\t" + rgb.getG() + "\t" + rgb.getB());
+        }
 	}
+}
+class RGB {
+    private int red;
+    private int green;
+    private int blue;
+    
+    public RGB(int red, int green, int blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+
+    public int getR() { return red; }
+    public int getG() { return green; }
+    public int getB() { return blue; }
 }
