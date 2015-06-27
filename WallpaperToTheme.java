7b3774fa702ebfc3c20f973858ea26176cfa6d10
araic <ciara.mcauley4@mail.dcu.ie>
2015-06-27 15:23:36 +0100

    Added comments and fixed bugs

--- WallpaperToTheme.java
+++ WallpaperToTheme.java
@@ -3,6 +3,7 @@ import javax.imageio.ImageIO;
 import java.io.File;
 import java.awt.Color;
 import java.io.IOException;
+import java.lang.*;
 
 class WallpaperToTheme {
     public static void main(String[] args){
@@ -24,30 +25,23 @@ class BufferedImaged {
 			int w = image.getWidth();
 			int h = image.getHeight();
 			int i = 0;
-		    int pixels = w*h;
+		    int pixels = w*h; //total num of pixels in image
 			int[] dataBuffInt = image.getRGB(0,0,w,h,null,0,w);
 			
-		    RGB[] colours = new RGB[pixels];
-		    Kmeans km = new Kmeans(colours, pixels);
-
-		    for (i=0; i<pixels; i++){
+		    RGB[] colours = new RGB[pixels]; //array for RGB of each pixel
+		    
+		    for (i=0; i<pixels; i++){ //grabbing RGB values from image
 				Color c = new Color(dataBuffInt[i]);
 				colours[i] = new RGB(c.getRed(),c.getGreen(),c.getBlue());
 		    }
-		    p("Red\tGreen\tBlue");
-		    for (i=0; i<pixels; i++){
-		    	RGB rgb = colours[i];
-		        p(rgb.getR() + "\t" + rgb.getG() + "\t" + rgb.getB());
-		    }
-        }
-	
+		    
+		    Kmeans km = new Kmeans(colours, pixels); //creating instance of kmeans
+		    km.calcingDistances();
 
-	private static void p(String s){
-		System.out.println(s);
-	}
+        }
 }
 
-class RGB {
+class RGB { //class to store RGB values
     private int red;
     private int green;
     private int blue;
@@ -64,44 +58,52 @@ class RGB {
 }
 
 
-class Kmeans {
+class Kmeans {//class to gather dominant colours
 	
-	RGB centroids[] = {new RGB(255,0,0),new RGB(0,255,0),new RGB(0,0,255)};//x2,y2,z2
-	float distance = 255;//max distance
-	float newDist = 0;
-	int returnVal = 4;
-	int[] closest = new int[pixels];
-	RGB colours[] = new RGB[];
+	int[] closest;//relates closest centroid to each pixel
+	RGB colours[];//colours of each pixel
 	int pixels = 0;
 
-	public Kmeans(RGB colours[], pixels){
+	public Kmeans(RGB colours[], int pixels){//overloaded constructor
 		this.colours = colours;
 		this.pixels = pixels;
+		closest = new int[pixels];
+		this.colours = colours;
 	}
-	
+
 	public void calcingDistances(){
-		for (i=0; i<pixels; i++){
+		RGB centroids[] = {new RGB(255,0,0),new RGB(0,255,0),new RGB(0,0,255)};//x2,y2,z2
+		for (int i=0; i<pixels; i++){
 			RGB rgb = colours[i];
-			closest[i] = km.calcDist(rgb);
-	   	}
-	   	
+			closest[i] = calcDist(rgb,centroids);
+		}
+		for (int i=0; i<pixels; i++){ //just printing rgb values of image
+	    	RGB rgb = colours[i];
+	        System.out.println(i + "\t" + rgb.getR() + "\t" + rgb.getG() + "\t" + rgb.getB() + "\t" + closest[i]);
+	    }
 	}
 	
-	public static float calcDist(RGB rgb){
-		float x1=rgb.getR();
-		float y1=rgb.getG();
-		float z1=rgb.getB();
+	public static int calcDist(RGB rgb,RGB centroids[]){
+		double distance = 100000;//max distance from a centroid to an RGB val of a pixel
+		double newDist = 0; //temp value for ordering clusters
+		
+		int returnVal = 4;//error testing
+		int x1=rgb.getR();
+		int y1=rgb.getG();
+		int z1=rgb.getB();
 		for(int i = 0; i < 3; i++){
-			float x2 = centroids[i].getR();
-			float y2 = centroids[i].getG();
-			float z2 = centroids[i].getB();
-			newDist = sqrt((x2-x1)^2+(y2-y1)^2+(z2-z1)^2);
+			int x2 = centroids[i].getR();
+			int y2 = centroids[i].getG();
+			int z2 = centroids[i].getB();
+			newDist = Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1))+((z2-z1)*(z2-z1)));
 			if(newDist < distance){
 				distance = newDist;
 				returnVal = i;
 			}
 		}
+		return returnVal;
 	}
 	
 
 }
+
